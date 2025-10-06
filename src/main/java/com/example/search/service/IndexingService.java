16691b9e6fea762c8e4d9c50c83c8ddd704cf927
Piyu-shh <patropiyush@gmail.com>
2025-10-06 18:31:34 +0530

    lazy annot added 2

--- src/main/java/com/example/search/service/IndexingService.java
+++ src/main/java/com/example/search/service/IndexingService.java
@@ -14,6 +14,7 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
